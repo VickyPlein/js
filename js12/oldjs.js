1b43305d2230c919941ee1168dc34b1a5ab177f5
VickyPlein <157711444+VickyPlein@users.noreply.github.com>
2024-04-09 17:23:08 +0300

    ts intro

---
+++ js12/oldjs.js
@@ -0,0 +1,6 @@
+console.log("Labas pasauli");
+let x1;
+let y2;
+x = "5";
+y = 6;
+console.log((x + y) * 2);
---
+++ js12/typescript_pradzia/labaspasauli.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log("Labas pasauli");
+let x;
+// Kintamasis yra skaičius, 33, 35.848, -4848
+let y;
+y = 0b11100;
+// Tekstinis tipas, string
+let s;
+s = "tekstas";
+// Masyvas
+let skaiciuMasyvas;
+skaiciuMasyvas = [5, 9, 6];
+let tekstoMasyvas;
+tekstoMasyvas = ["a", "b"];
+// Boolean - tiesa arba melas
+let veikia;
+veikia = true;
+veikia = false;
+x = 9;
+y = 6;
+console.log(x + y);
