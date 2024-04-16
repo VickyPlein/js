2a56193a2709f06a945f197b6b085e52c9d4e956
VickyPlein <157711444+VickyPlein@users.noreply.github.com>
2024-04-16 15:24:35 +0300

    upd

---
+++ ts3/src/studentas.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Studentas = void 0;
+class Studentas {
+    constructor() {
+        this.vardas = "";
+        this.pavarde = "";
+    }
+}
+exports.Studentas = Studentas;
---
+++ ts3/src/university.js
@@ -0,0 +1,15 @@
+"use strict";
+/*{
+    "name": "Medical Academy of Latvia",
+    "domains": [
+      "aml.lv"
+    ],
+    "state-province": null,
+    "country": "Latvia",
+    "alpha_two_code": "LV",
+    "web_pages": [
+      "http://www.aml.lv/"
+    ]
+  },
+  */
+Object.defineProperty(exports, "__esModule", { value: true });
